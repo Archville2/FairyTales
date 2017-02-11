cd80f80e6aea5805fa3c40f400b91a4cea64a5cb
Nik <qbk@tut.by>
2017-02-11 10:07:04 +0300

    patching errors

--- by/htp/lesson05/task03/service/impl/MaxPagesService.java
+++ by/htp/lesson05/task03/service/impl/MaxPagesService.java
@@ -4,6 +4,7 @@ import by.htp.lesson05.task03.bean.RequestDTO;
 import by.htp.lesson05.task03.bean.ResultDTO;
 import by.htp.lesson05.task03.dao.CommandsDAO;
 import by.htp.lesson05.task03.dao.factory.DAOFactory;
+import by.htp.lesson05.task03.dao.exception.DAOException;
 import by.htp.lesson05.task03.service.exception.ServiceException;
 
 public class MaxPagesService {
@@ -23,7 +24,7 @@ public class MaxPagesService {
 
 		try {
 			return commandsDAO.maxPages(requestDTO);
-		} catch (Exception e) {
+		} catch (DAOException e) {
 			throw new ServiceException(e);
 		}
 	}
